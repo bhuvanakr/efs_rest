3cc0b8d7a72fa94246c66560c83269bb73bcb5f4
bhuvi <bhuvionline@live.com>
2021-03-14 17:58:49 -0600

    Updated Procfile location

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'efs_rest.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'efs',
@@ -92,14 +92,14 @@ DATABASES = {
         'PASSWORD': 'admin',
         'PORT': '5433'
     }
-}
+}'''
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
+}
 
 
 # Password validation
